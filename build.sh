c945fef99560f2b88441014b1b7a725e9c863ff1
Ellen Marie Dash <me@duckie.co>
2010-03-06 20:04:33 -0500

    I definitely edited all of this, then forgot to commit

--- build.sh
+++ build.sh
@@ -1,35 +1,9 @@
-AS=nasm
-ASFLAGS=-fbin
-
-OBJDIR="obj/boot"
-SRCDIR="src/boot"
-ISOBOOT="boot"
-LOADERNAME="loader"
-ISOFILE="cd.iso"
-
-if [ "-g" == "$1" ]; then
-	ASDEBUG=" -g "
-else
-	ASDEBUG=""
-fi
-
 if [ "clean" == "$1" ]; then
-	rm -r obj
-	rm -r isofs
-	rm ${ISOFILE}
+	rm -rf isofs tiny tiny.iso
 	exit
 fi
 
-function _ {
-	echo $@
-	$@
-}
+mkdir -p isofs
 
-mkdir -p obj/boot
-mkdir -p isofs/${ISOBOOT}
-
-_ ${AS} ${ASDEBUG} ${ASFLAGS} -o ${OBJDIR}/${LOADERNAME}.o ${SRCDIR}/${LOADERNAME}.asm
-
-_ cp ${OBJDIR}/${LOADERNAME}.o isofs/${ISOBOOT}/${LOADERNAME}
-
-_ genisoimage -r -b ${ISOBOOT}/${LOADERNAME} -no-emul-boot -boot-load-size 4 -o ${ISOFILE} isofs
+nasm -g -fbin -o isofs/tiny tiny.asm
+genisoimage -r -b tiny -no-emul-boot -boot-load-size 4 -o tiny.iso isofs
---
+++ tiny.asm
@@ -0,0 +1,14 @@
+bits 16
+org 7C00h
+
+mov ah, 0xe
+
+mov si, string
+print:
+	lodsb
+	or al, al
+	jz $
+	int 0x10
+	jmp print
+
+string  db  "Hello, world!", 0
